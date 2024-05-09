2eeb437b421e7866a9800b77533a3b8cefb4b35c
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-05-09 20:22:42 +0200

    added dynamic page loading for menu

--- src/components/menu/menunavbar.jsx
+++ src/components/menu/menunavbar.jsx
@@ -1,43 +1,22 @@
-export default function MenuNavbar() {
+import React from "react";
+
+export default function MenuNavbar({
+  categories,
+  onCategoryChange,
+  activeCategoryId,
+}) {
   return (
-    <nav className="w-full p-4 border-b">
-      <ul className="flex justify-between items-end text-gray-400">
-        <li className="font-bold text-black border-b-2 border-orange-500 pb-2">
-          <a
-            href="#"
-            onClick={(e) => e.preventDefault()}
-            style={{ color: "inherit", textDecoration: "none" }}
+    <nav className="w-full p-4 border-b overflow-x-auto">
+      <ul className="flex justify-start items-center space-x-4">
+        {categories.map((category) => (
+          <li
+            key={category.id}
+            className={`font-bold text-black hover:text-orange-500 cursor-pointer ${category.id === activeCategoryId ? "border-b-2 border-orange-500 pb-2" : ""}`}
+            onClick={() => onCategoryChange(category.id)}
           >
-            Food
-          </a>
-        </li>
-        <li className="hover:text-black">
-          <a
-            href="#"
-            onClick={(e) => e.preventDefault()}
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            Alcohol
-          </a>
-        </li>
-        <li className="hover:text-black">
-          <a
-            href="#"
-            onClick={(e) => e.preventDefault()}
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            Soft drinks
-          </a>
-        </li>
-        <li className="hover:text-black">
-          <a
-            href="#"
-            onClick={(e) => e.preventDefault()}
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            Other
-          </a>
-        </li>
+            {category.name}
+          </li>
+        ))}
       </ul>
     </nav>
   );
