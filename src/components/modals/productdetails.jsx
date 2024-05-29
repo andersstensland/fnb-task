f3af8cc5c9dd316657f4cbdd86e52f24d98d72c1
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-05-29 09:54:32 +0200

    updated css in productdetails

--- src/components/modals/productdetails.jsx
+++ src/components/modals/productdetails.jsx
@@ -5,43 +5,21 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Label } from "@/components/ui/label";
-import { Checkbox } from "@/components/ui/checkbox";
+import ToppingItem from "../productdetails/toppingitem";
+import { Button } from "../ui/button";
 
 const ProductDetailsModal = ({ item, isOpen, onClose }) => {
   const [size, setSize] = useState("Medium");
   const [quantity, setQuantity] = useState(1);
   const [toppings, setToppings] = useState([]);
 
-  const toggleTopping = (topping) => {
-    setToppings((prev) => {
-      const currentIndex = prev.indexOf(topping);
-      if (currentIndex === -1) {
-        return [...prev, topping];
-      } else {
-        return prev.filter((item) => item !== topping);
-      }
-    });
-  };
-
   // Toggle body scroll on modal open/close
   useEffect(() => {
-    const body = document.body;
-    if (isOpen) {
-      body.style.overflow = "hidden";
-    } else {
-      body.style.overflow = "visible";
-    }
-
-    // Clean up function to reset overflow when component unmounts or modal closes
+    document.body.style.overflow = isOpen ? "hidden" : "visible";
     return () => {
-      body.style.overflow = "visible";
+      document.body.style.overflow = "visible"; // Reset on component unmount
     };
   }, [isOpen]);
-
-  const handleSizeChange = (event) => {
-    setSize(event.target.value);
-  };
 
   const handleQuantityChange = (delta) => {
     let newQuantity = quantity + delta;
@@ -56,9 +34,11 @@ const ProductDetailsModal = ({ item, isOpen, onClose }) => {
     return 175 + toppings.reduce((acc, curr) => acc + curr.cost, 0);
   }
 
+  function handleAddBasket() {}
+
   return isOpen ? (
     <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-start pt-32">
-      <div className="bg-white rounded-lg w-full max-w-lg mx-auto overflow-auto h-full">
+      <div className="bg-white rounded-lg w-full max-w-lg mx-auto overflow-y-auto h-full">
         <button onClick={onClose} className="absolute top-22.5 right-0 p-4">
           ✖️
         </button>
@@ -73,13 +53,13 @@ const ProductDetailsModal = ({ item, isOpen, onClose }) => {
           </div>
           <div className="flex space-x-2">
             {["Small", "Medium", "Large"].map((s) => (
-              <button
+              <Button
                 key={s}
                 className={`py-1 px-4 border ${size === s ? "border-black" : "border-transparent"}`}
                 onClick={() => setSize(s)}
               >
                 {s}
-              </button>
+              </Button>
             ))}
           </div>
           <div className="text-xl font-semibold mt-2">{totalCost} NOK</div>
@@ -94,82 +74,46 @@ const ProductDetailsModal = ({ item, isOpen, onClose }) => {
           </div>
           <div className="mt-4">
             <h3 className="font-semibold">Remove topping:</h3>
-            {Object.keys(item.content).map((toppingKey) => (
-              <div
-                key={toppingKey}
-                className="flex justify-between items-center rounded-md"
-                onChange={() => toggleTopping({ name: toppingKey, cost: 10 })}
-                checked={toppings.includes({ name: toppingKey, cost: 10 })}
-              >
-                <Label
-                  key={toppingKey}
-                  className="flex justify-between items-center"
-                  onChange={() => toggleTopping({ name: toppingKey, cost: 10 })}
-                  checked={toppings.includes({ name: toppingKey, cost: 10 })}
-                >
-                  {toppingKey}
-                </Label>
-                <Checkbox id="terms" className="mt-2" />
-              </div>
-            ))}
+            <ToppingItem item={item} topping={item.content} />
           </div>
           <div className="mt-4">
             <h3 className="font-semibold">Extra topping:</h3>
-            {Object.keys(item.content).map((toppingKey) => (
-              <div key={toppingKey} className="flex justify-between">
-                <Label
-                  key={toppingKey}
-                  className="flex justify-between items-center rounded-md"
-                  onChange={() => toggleTopping({ name: toppingKey, cost: 10 })}
-                  checked={toppings.includes({ name: toppingKey, cost: 10 })}
-                >
-                  {toppingKey}
-                </Label>
-                <Checkbox id="terms" className="mt-2" />
-              </div>
-            ))}
+            <ToppingItem item={item} topping={item.content} />
           </div>
           <div className="mt-4">
             <h3 className="font-semibold">Add sides:</h3>
-            {Object.keys(item.content).map((toppingKey) => (
-              <div key={toppingKey} className="flex justify-between">
-                <Label
-                  key={toppingKey}
-                  className="flex justify-between items-center rounded-md"
-                  onChange={() => toggleTopping({ name: toppingKey, cost: 10 })}
-                  checked={toppings.includes({ name: toppingKey, cost: 10 })}
-                >
-                  {toppingKey}
-                </Label>
-                <Checkbox id="terms" className="mt-2" />
-              </div>
-            ))}
+            <ToppingItem item={item} topping={toppings} />
           </div>
         </div>
 
-        <div className="fixed w-full p-4 justify-between items-center bg-orange-300 bottom-0 rounded-t-xl">
-          <div className="">
-            <span>Antall</span>
-            <span>{totalCost}</span>
+        <div className="fixed w-full p-4 bg-customOrange bottom-0 rounded-t-xl">
+          <div className="flex justify-between">
+            <div className="flex flex-col my-2">
+              <span>Antall</span>
+              <span>{totalCost}</span>
+            </div>
+            <div className="flex items-center">
+              <Button
+                onClick={() => handleQuantityChange(-1)}
+                className="text-xl bg-customSecondaryOrange"
+              >
+                −
+              </Button>
+              <span className="mx-4">{quantity}</span>
+              <Button
+                onClick={() => handleQuantityChange(1)}
+                className="text-xl bg-customSecondaryOrange"
+              >
+                +
+              </Button>
+            </div>
           </div>
-          <div className="flex items-center">
-            <button
-              onClick={() => handleQuantityChange(-1)}
-              className="text-2xl"
-            >
-              −
-            </button>
-            <span className="mx-4">{quantity}</span>
-            <button
-              onClick={() => handleQuantityChange(1)}
-              className="text-2xl"
-            >
-              +
-            </button>
-          </div>
-          <button className="bg-white text-black px-8 py-2 rounded-md w-full">
+          <Button
+            onClick={() => handleAddBasket()}
+            className="bg-white text-black px-8 py-2 rounded-md w-full"
+          >
             Add to basket
-          </button>
+          </Button>
         </div>
       </div>
     </div>
