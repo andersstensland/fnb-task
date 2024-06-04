695db3276d24f7ac676e726d4c77a392cae8d0f6
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-06-04 11:02:28 +0200

    remove cart item and fetchcart

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,5 @@ import menuCategory from './menuCategory'
 import menuSubcategory from './menuSubcategory'
 import menuItem from './menuItem'
 import imageAsset from './imageAssets'
-import cart from './cart'
-import cartItem from './cartItem'
 
-export const schemaTypes = [menuCategory, menuSubcategory, menuItem, imageAsset, cart, cartItem]
+export const schemaTypes = [menuCategory, menuSubcategory, menuItem, imageAsset]
