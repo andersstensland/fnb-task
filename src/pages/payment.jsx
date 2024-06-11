ea83b2978ddc656dd21db4647e6fb9a5c344c6bb
Strilakaren <thas005@student.kristiania.no>
2024-06-11 14:00:52 +0200

    minor adjustment to top elements

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -30,17 +30,19 @@ export default function Payment() {
       <Navbar />
       <div className="flex flex-col items-center min-h-screen px-4 w-full">
         <PaymentMethodModal />
-        <div className="w-full max-w-4xl mx-auto">
-          <div className="flex items-center">
-            <Button onClick={handleBack} className="mr-4" variant="ghost">
+        <div className="relative w-full max-w-4xl mx-auto">
+          <div className="flex justify-center items-center relative">
+            <Button
+              onClick={handleBack}
+              className="absolute left-0 p-0 w-10 h-10 flex items-center justify-center" // Ensure the button is positioned at the left edge and is large enough
+              variant="ghost">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6"
-              >
+                className="w-6 h-6">
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -48,65 +50,68 @@ export default function Payment() {
                 />
               </svg>
             </Button>
-            <h1 className="text-xl font-bold my-4 text-center">
-              Order Summary
-            </h1>
+            <h1 className="text-xl font-bold my-4 mx-auto">Order Summary</h1>
           </div>
-          <div className="w-full mb-4">
-            <Label className="block text-sm font-bold mb-2">Delivery</Label>
-            <RadioGroup defaultValue="option-one">
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-one" id="option-one" />
-                <Label htmlFor="option-one">
-                  As soon as possible (25-35min)
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-two" id="option-two" />
-
-                <DeliveryTimeModal
-                  trigger={
-                    <Label htmlFor="option-two">Choose time to pre-order</Label>
-                  }
-                />
-              </div>
-            </RadioGroup>
-          </div>
-
-          <div className="w-full mb-4">
-            <Label className="block text-sm font-bold mb-2">Payment</Label>
-            <div className="w-full mb-4">
-              <Label className="block text-sm font-bold mb-2">
-                Choose where you want to eat!
-              </Label>
-              <div className="flex flex-col md:flex-row md:items-center justify-between md:space-x-4">
-                <Button
-                  className="bg-white text-black w-full md:w-40 px-8 py-4 text-xl font-semibold border-2 border-orange-300 rounded-lg focus:bg-orange-300 focus:text-white focus:border-transparent"
-                  onClick={() => handleOptionClick("delivery")}
-                >
-                  Delivery
-                </Button>
-                <PickupModal />
-              </div>
+        </div>
+        <div className="w-full mb-4">
+          <Label className="block text-sm font-bold mb-2">Delivery</Label>
+          <RadioGroup defaultValue="option-one">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="option-one"
+                id="option-one"
+              />
+              <Label htmlFor="option-one">As soon as possible (25-35min)</Label>
             </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="option-two"
+                id="option-two"
+              />
 
-            <OrderSummary />
-
-            <div className="w-full mb-4">
-              <Label className="block text-sm font-bold mb-2" htmlFor="message">
-                Leave a message (optional)
-              </Label>
-              <Textarea
-                id="message"
-                className="w-full h-24 p-2 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500 shadow-sm"
-                value={""}
-                onChange={(e) => setMessage(e.target.value)}
-                placeholder="Write your message here..."
+              <DeliveryTimeModal
+                trigger={
+                  <Label htmlFor="option-two">Choose time to pre-order</Label>
+                }
               />
             </div>
+          </RadioGroup>
+        </div>
 
-            {/* Pass deliveryOption, deliveryCost, and items to Paymentsummary */}
+        <div className="w-full mb-4">
+          <Label className="block text-sm font-bold mb-2">Payment</Label>
+          <div className="w-full mb-4">
+            <Label className="block text-sm font-bold mb-2">
+              Choose where you want to eat!
+            </Label>
+            <div className="flex flex-col md:flex-row md:items-center justify-between md:space-x-4">
+              <Button
+                className="bg-white text-black w-full md:w-40 px-8 py-4 text-xl font-semibold border-2 border-orange-300 rounded-lg focus:bg-orange-300 focus:text-white focus:border-transparent"
+                onClick={() => handleOptionClick("delivery")}>
+                Delivery
+              </Button>
+              <PickupModal />
+            </div>
           </div>
+
+          <OrderSummary />
+
+          <div className="w-full mb-4">
+            <Label
+              className="block text-sm font-bold mb-2"
+              htmlFor="message">
+              Leave a message (optional)
+            </Label>
+            <Textarea
+              id="message"
+              className="w-full h-24 p-2 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500 shadow-sm"
+              value={""}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Write your message here..."
+            />
+          </div>
+
+          {/* Pass deliveryOption, deliveryCost, and items to Paymentsummary */}
         </div>
       </div>
     </>
