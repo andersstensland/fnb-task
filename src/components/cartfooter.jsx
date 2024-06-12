3f2836d5ef00e18ba0c2d334e40b4db533220502
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-06-12 12:25:19 +0200

    updated css cartfooter

--- src/components/cartfooter.jsx
+++ src/components/cartfooter.jsx
@@ -9,7 +9,7 @@ export default function CartFooter() {
 
   return (
     <>
-      <div className="fixed bottom-2 left-2 right-2 bg-teal-500 p-4 flex justify-between items-center text-black shadow-md rounded-md md:mx-auto md:max-w-3xl">
+      <div className="fixed bottom-2 left-2 right-2 bg-teal-500 p-4 flex justify-between items-center text-black shadow-md rounded-md md:mx-auto md:max-w-2xl">
         <div className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
